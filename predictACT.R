7b84daa0dab5b37eba59edb8271956f326f1b591
btcarter <benjamin.t.carter@gmail.com>
2019-07-02 15:45:27 -0600

    adding predictive analysis

---
+++ predictACT.R
@@ -0,0 +1,30 @@
+# preamble
+#   author: Benjamin Carter
+#   date: 2019-07-01
+#   project: Skilled Reading Project
+#   objective: predict ACT score based on eye tracking measures of reading
+
+# environment
+STUDY="/Box/LukeLab/SkilledReadingStudy" # path to study directory
+ANAL="/Dropbox/Lab data & Papers/analyses/predictACT" # path to analysis directory
+WORK="~/Box/LukeLab/SkilledReadingStudy/results/predictACT" # path to working directory
+ET_DATA="~/Box/LukeLab/SkilledReadingStudy/data/eyeTrackingData/allRuns.csv" # path for eye tracking dataframe
+SUMM_DATA="~/Box/LukeLab/SkilledReadingStudy/data/demographics/participantDemographicsWithETStats.csv" # path for summary dataframe
+OUT_NAME="results.txt" # destination file name for output
+list.of.packages <- c() # list of packages to use in analysis
+
+# load libraries
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]                           # compare the list to the installed packages list and add missing packages to new list
+if(length(new.packages)) install.packages(new.packages, dependencies = TRUE)                                          # install missing packages
+lapply(list.of.packages,library,character.only = TRUE)                                                                # load packages
+
+
+# functions
+
+# preprocessing
+  
+# predict
+  
+# evaluate
+
+# output?
