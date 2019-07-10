aa127bddf85ecc33d2dac2eeb91d05460e10e28d
btcarter <benjamin.t.carter@gmail.com>
2019-07-10 16:54:30 -0600

    trying to make predictions work, also model may be too complicated

--- predictACT.R
+++ predictACT.R
@@ -10,8 +10,8 @@ ANAL="~/Dropbox/Lab data & Papers/analyses/predictACT" # path to analysis direct
 WORK="~/Box/LukeLab/SkilledReadingStudy/results/predictACT" # path to working directory
 ET_DATA="~/Box/LukeLab/SkilledReadingStudy/data/eyeTrackingData/allRuns.csv" # path for eye tracking dataframe
 SUMM_DATA="~/Box/LukeLab/SkilledReadingStudy/data/demographics/participantDemographicsWithETStats.csv" # path for summary dataframe
-OUT_NAME="results.txt" # destination file name for output
-list.of.packages <- c("stats","lme4","lmerTest") # list of packages to use in analysis
+OUT_NAME="predictActResults.txt" # destination file name for output
+list.of.packages <- c("lme4") # list of packages to use in analysis
 
 # load libraries
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]                           # compare the list to the installed packages list and add missing packages to new list
@@ -28,43 +28,28 @@ makeParticipantList <- function(OCULO.DF,SUMM.DF) {
 }
 
 #   add ACT score to ET_DATA and then select only participants with an ACT
-addACT <- function(OCULO.DF,SUMM.DF,LIST,PARTICIPANT) {
+predictACT <- function(OCULO.DF,SUMM.DF,LIST,PARTICIPANT) {
   for (participant in LIST) {
     score <- SUMM.DF[["actScore"]][SUMM.DF[["recording_session_label"]]==participant]
     regressions <- SUMM.DF[["REGRESSIONS"]][SUMM.DF[["recording_session_label"]]==participant]
     OCULO.DF[["actScore"]][OCULO.DF[["RECORDING_SESSION_LABEL"]]==participant] <- score
     OCULO.DF[["REGRESSIONS"]][OCULO.DF[["RECORDING_SESSION_LABEL"]]==participant] <- regressions
   }
-  modelDF <- OCULO.DF[!is.na(OCULO.DF[["actScore"]])==TRUE & OCULO.DF[["RECORDING_SESSION_LABEL"]]!=PARTICIPANT]
-  testDF <- OCULO.DF[OCULO.DF[["RECORDING_SESSION_LABEL"]]==PARTICIPANT]
-  return_list <- list(modelDF,testDF)
-  return(return_list)
+  modelDF <- OCULO.DF[ which(!is.na(OCULO.DF[["actScore"]])==TRUE & OCULO.DF[["RECORDING_SESSION_LABEL"]]!=PARTICIPANT), ]                                      # dataframe without participant of interest's data
+  testDF <- OCULO.DF[OCULO.DF[["RECORDING_SESSION_LABEL"]]==PARTICIPANT, ]                                                                                        # dataframe only with participant of interest's data
+  #formula = lmer(actScore ~ log(CURRENT_FIX_DURATION) + log(NEXT_SAC_AMPLITUDE) + log(REGRESSIONS) + (1|RUN) + (1|RECORDING_SESSION_LABEL), data=modelDF)                      # model
+  formula = lmer(actScore ~ log(CURRENT_FIX_DURATION) + log(NEXT_SAC_AMPLITUDE) + REGRESSIONS+ (1|RUN) + (1|RECORDING_SESSION_LABEL), data=modelDF)
+  testDF$predictedACT <- predict(testDF,formula)                                                                                                                         # make a prediction
+  return(prediction)                                                                                                                                            # spit it out
 }
-
-#   function to make a model.
-model <- function(INPUT){
-  score = lmer(actScore ~ CURRENT_FIX_DURATION + NEXT_SAC_AMPLITUDE + REGRESSIONS + (1|RUN) + (1|RECORDING_SESSION_LABEL), data=INPUT)
-  return(score)
-}
-
-#   function to predict ACT based on model
-predictACT <- function(PARTICIPANT_DATA,MODEL){
-  act <- predict(PARTICIPANT_DATA,MODEL)
-  return(act)
-}
-
 
 #   function to evaluate predictions
 evalPrediction <- function(true,predicted){
   R_sq=cor(true,predicted,use = "pairwise.complete.obs")^2
-  return(r_sq)
+  return(R_sq)
 }
 
-#   function to execute above functions and output data?
-order66 <- function(INPUT){
-  #all the things
-}
-  
+
 #### EXECUTE ####
 
 # read in dataframes
@@ -73,12 +58,10 @@ SUMM_DATA <- read.csv(SUMM_DATA,header=TRUE,sep=",",na.strings = "NA")
 
 # make predictions
 participants <- makeParticipantList(ET_DATA,SUMM_DATA)
-for (person in participants) {
-  data <- addACT(ET_DATA,SUMM_DATA,participants,person)
-  
+for (i in participants){
+  p <- predictACT(ET_DATA,SUMM_DATA,participants,i)
+  SUMM_DATA[["predACT"]][SUMM_DATA[["recording_session_label"]]==i] <- predictACT(ET_DATA,SUMM_DATA,participants,i)
 }
 
-
-# evaluate predictions
-
-# save output?
+# run an R-squared
+rval <- evalPrediction(SUMM_DATA[["actScore"]],SUMM_DATA[["predACT"]])
